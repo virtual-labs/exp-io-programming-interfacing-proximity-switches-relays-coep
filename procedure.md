0d554170a6eba6822cb2f4958204f4dde8815141
vleadadmin <admin@vlabs.ac.in>
2025-11-25 10:44:58 +0000

    https://virtual-labs.github.io/exp-io-programming-interfacing-proximity-switches-relays-coep click on the link to test your code.

---
+++ procedure.md
@@ -0,0 +1,30 @@
+##  Procedure
+
+#### Configuration
+- Select input Pin For the sensor
+- Select output Pin For for the DC Motor
+- Select the Sensor Type (Inductive or Capacitive)
+- Select object material
+- Click on "GENERATE CODE" button to generate code
+- Click on "COMPILE" button to compile code
+- Click on "EXECUTE" button to execute code and goto mimic
+- If you want to download code click on "DOWNLOAD" button
+- The MIMIC will open on left side of screen
+
+#### Simulation
+- Connect the Output Pin from the Arduino to the transistor by click on the connecting points
+- Connect the selected Input Pin from the Arduino to the sensor by click on the connecting points
+- Connect the relay and DC motor according to the circuit diagram by click on the connecting points
+- Connect the relay and power supply according to the circuit diagram by click on the connecting points
+- Click on "Check Connection" to validate the circuit connections
+- If connections are shows wrong then you can click on the particular connected line to delete the wrong connection (wrong connection shows in RED color)
+- After completion of circuit click on check connection object will displayed in front of sensor
+- Click on "Plus" and "Minus" buttons  for movement the object 
+- When object reached in between the sensing range of the sensor then current will flows through the circuit and motor starts running 
+- Observe the changes in circuit and rotation of motor while changing the position of object from sensor 
+- Click on "Next Level" button for next steps 
+
+#### Questions
+- Read all the questions carefully and submit the answer
+- After submission, verify the answers 
+- Click on result button, "Result" page will displayed
---
+++ simulation/bin/questionJson.js
@@ -0,0 +1,143 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "Inductive proximity sensors are mainly used to detect:",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Metallic objects"
+					}, {
+						"ANSID": false,
+						"content": "Plastic objects"
+					}, {
+						"ANSID": false,
+						"content": "Wooden objects"
+					},{
+						"ANSID": false,
+						"content": "Glass objects "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": "Capacitive proximity sensors can detect",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Only metals"
+					}, {
+						"ANSID": true,
+						"content": "Metals and non-metals "
+					}, {
+						"ANSID": false,
+						"content": "Only wood "
+					}, {
+						"ANSID": false,
+						"content": "Only glass"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Typical sensing range of inductive proximity sensors is:",
+					"ANS": [{
+						"ANSID": true,
+						"content": "0.5 to 10 mm"
+					}, {
+						"ANSID": false,
+						"content": "50 to 200 mm"
+					}, {
+						"ANSID": false,
+						"content": "1 to 5 meters"
+					}, {
+						"ANSID": false,
+						"content": "More than 10 meters"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Typical sensing range of capacitive sensors is ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "0.5 to 10 mm"
+					}, {
+						"ANSID": true,
+						"content": " 2 to 50 mm"
+					}, {
+						"ANSID": false,
+						"content": "1 to 5 meters"
+					}, {
+						"ANSID": false,
+						"content": "5 to 10 meters"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which parameter affects the sensing distance of inductive sensors?",
+					"ANS": [{
+						"ANSID": false,
+						"content": " Temperature of the air"
+					}, {
+						"ANSID": false,
+						"content": "Humidity in the environment"
+					}, {
+						"ANSID": true,
+						"content": "Material of the target"
+					}, {
+						"ANSID": false,
+						"content": "Color of the target"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Inductive sensors are not suitable for detecting",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Iron"
+					}, {
+						"ANSID": true,
+						"content": "Wood"
+					}, {
+						"ANSID": false,
+						"content": "Copper"
+					}, {
+						"ANSID": false,
+						"content": "Steel"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
